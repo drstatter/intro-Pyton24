7634dbcbffc752d845cff501b5b7848eb4915d99
david <statter@gmail.com>
2024-11-27 15:21:49 +0200

    1

---
+++ class3_if/sign.py
@@ -0,0 +1,7 @@
+letter="3"
+if "A"<=letter<="z":
+    print("The letter is a  letter")
+elif "0"<=letter<="9":
+    print("The letter is a number")
+else:
+    print("The letter is not a letter or a number")
